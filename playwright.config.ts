db0145716f23a50b28007ef254046b46031d8df5
amulya <amulyagowda1996@gmail.com>
2026-09-25 12:04:47 +0530

    Modified cron

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,13 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  //reporter: [['html',{open: 'always',outputFolder:'my-reports'}]],
+  //reporter:'line',
+  //reporter:'dot',
+ // reporter:[['json',{outputFile:'myreports/results.json'}]],
+ //reporter:[['junit',{outputFile:'myreports/results.xml'}]],
+ reporter:[['allure-playwright',{outputFolder:"allure-results"}]],
+      
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
